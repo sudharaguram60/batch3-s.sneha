f98b4f5f62f8f2985dc5aba69b7f27479e756899
sudharaguram60 <162982882+sudharaguram60@users.noreply.github.com>
2024-03-11 16:05:57 +0530

    Add files via upload

---
+++ sneha.py
@@ -0,0 +1,39 @@
+#print("hello world")
+print("hello")
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+'''
+import this
+print(this)
+import antigravity
+''''
+BARGAINED CHARRACTERS
+contrlos and data structures-ABC language
+string and its for mats-from C
+regular expression-pearL
+Expception handling-modules 3
+magic methods from-fortron
+,,,
+WHAT KIND OF LANGUAGE IS PYTHON
+pyton is
+*object orinted programming language
+*dyanmiccally typed language
+*scripting language
+*interpreted language
+*complied language
+*functional programming language
+
+
+#!---->variables
+#variable is a name which is used to identify the value of variable
+#?which is strord in a specific memory location
+#! RULES to dedfine variables
+1)variable have to start with only alphets, underscores
+
+a=2
+b=3
+c=23
+print(str(a),end
+
